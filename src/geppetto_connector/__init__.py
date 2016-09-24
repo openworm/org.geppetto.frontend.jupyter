202a329490e11919fbe3f0edc677e0c39b142e46
Adrian Quintana <adrianquintana@gmail.com>
2016-09-24 12:37:28 +0100

    creating web socket, access to geppetto files and rendering geppetto template

--- src/geppetto_connector/__init__.py
+++ src/geppetto_connector/__init__.py
@@ -1,5 +1,9 @@
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
+import os
+import tornado.websocket
+import tornado.web
+import json
 
 def _jupyter_server_extension_paths():
     return [{
@@ -13,15 +17,27 @@ def _jupyter_nbextension_paths():
         # the path is relative to the `my_fancy_module` directory
         src="static/geppetto_connector",
         # directory in the `nbextension/` namespace
-        dest="geppetto_connector",
-        # _also_ in the `nbextension/` namespace
-        require="geppetto_connector/index")]
+        dest="geppetto_connector")]
     
 class HelloWorldHandler(IPythonHandler):
     def get(self):
-        self.finish('Hello, world!')
+        #self.write(self.render_template('./static/geppetto/templates/dist/geppetto.vm'))
+        self.write(open(os.path.join(os.path.dirname(__file__), 'static/geppetto/templates/dist/geppetto.vm')).read())
 
-
+     
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        self.write_message({"type":"client_id","data":"{\"clientID\":\"Connection161\"}"})
+ 
+    def on_message(self, message):
+        jsonMessage = json.loads(message)
+        if (jsonMessage['type'] == 'geppetto_version'):
+            #Where do we get the geppetto version from?
+            self.write_message({"requestID":jsonMessage['requestID'],"type":"geppetto_version","data":"{\"geppetto_version\":\"0.3.1\"}"});
+ 
+    def on_close(self):
+        pass      
+      
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("my module enabled!")
     
@@ -29,4 +45,10 @@ def load_jupyter_server_extension(nbapp):
     host_pattern = '.*$'
     route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
     web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
+    
+    websocket_pattern = url_path_join(web_app.settings['base_url'], '/GeppettoServlet')
+    web_app.add_handlers(host_pattern, [(websocket_pattern, WebSocketHandler)])
+    
+    web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(__file__), 'static/geppetto/')})])
+    
     
