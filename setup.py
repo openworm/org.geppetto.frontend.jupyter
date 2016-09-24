d68f942c06c60e8a6b42ba80192ca722f1bfca90
Adrian Quintana <adrianquintana@gmail.com>
2016-09-24 12:36:10 +0100

    providing access to geppetto files

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 import setuptools
+from glob import glob
+
+import fnmatch
+import os
+
+data_files = []
+for root, dirnames, filenames in os.walk('src/geppetto_connector/static/geppetto/css/'):
+    for filename in fnmatch.filter(filenames, '*'):
+        data_files.append(('share/jupyter/nbextensions' + root[3:], [os.path.join(root, filename)]))
+for root, dirnames, filenames in os.walk('src/geppetto_connector/static/geppetto/less/'):
+    for filename in fnmatch.filter(filenames, '*'):
+        data_files.append(('share/jupyter/nbextensions' + root[3:], [os.path.join(root, filename)]))
+for root, dirnames, filenames in os.walk('src/geppetto_connector/static/geppetto/images/'):
+    for filename in fnmatch.filter(filenames, '*'):
+        data_files.append(('share/jupyter/nbextensions' + root[3:], [os.path.join(root, filename)]))
+for root, dirnames, filenames in os.walk('src/geppetto_connector/static/geppetto/js/'):
+    for filename in fnmatch.filter(filenames, '*'):
+        data_files.append(('share/jupyter/nbextensions' + root[3:], [os.path.join(root, filename)]))
+        
+# print(data_files)
+
 
 setuptools.setup(
     name="geppetto_connector",
@@ -9,5 +30,16 @@ setuptools.setup(
     long_description=open('README.md').read(),
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
+    data_files=data_files,
     include_package_data=True,
 )
+
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/css',glob('src/geppetto_connector/static/geppetto/css/*.css')),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/less',glob('src/geppetto_connector/static/geppetto/less/*.less')),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/js',glob('src/geppetto_connector/static/geppetto/js/*.js')),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/js/vendor',glob('src/geppetto_connector/static/geppetto/js/vendor/*.js')),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/images',glob('src/geppetto_connector/static/geppetto/images/*.png'))
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/css',cssmatches),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/less',lessmatches),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/js',jsmatches),
+#                  ('share/jupyter/nbextensions/geppetto_connector/geppetto/images',imagesmatches)
