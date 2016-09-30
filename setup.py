2bb95b216dc15df4e6e1037e6cd6dc86fd0e97b6
Adrian Quintana <adrianquintana@gmail.com>
2016-09-30 15:42:46 +0100

    added css and initial work on parameterization

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 from glob import glob
 import fnmatch
 import os
+from setuptools.command.install import install
 
 #This block copies resources to the server (/usr/local/share/jupyter/nbextensions/)
 data_files = []
@@ -19,11 +20,22 @@ for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/js/'):
         data_files.append(('share/jupyter/nbextensions' + root[3:], [os.path.join(root, filename)]))
 
 data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.js')))
+data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.css')))
+class CustomInstallCommand(install):
+    """Customized setuptools install command - prints a friendly greeting."""
+    def run(self):
+        print("Hello, developer, how are you? :)")
+        print(self)
+        print(self.prefix)
+        install.run(self)
 
 #raise AttributeError(data_files) For debug purposes
 
 setuptools.setup(
     name="geppettoJupyter",
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
     version='0.1.0',
     url="http://geppetto.org",
     author="Adrian Quintana",
@@ -34,3 +46,6 @@ setuptools.setup(
     data_files=data_files,
     include_package_data=True,
 )
+
+
+#--install-option="--prefix='/usr/local'"
