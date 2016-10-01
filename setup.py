5b81d6a15894066d8af79263688bb283b0e51fd5
Adrian Quintana <adrianquintana@gmail.com>
2016-10-01 20:52:33 +0100

    add option to configure componentsinitialization from pip install

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from glob import glob
 import fnmatch
 import os
 from setuptools.command.install import install
+import fileinput
 
 #This block copies resources to the server (/usr/local/share/jupyter/nbextensions/)
 data_files = []
@@ -21,13 +22,33 @@ for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/
 
 data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.js')))
 data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.css')))
+
 class CustomInstallCommand(install):
-    """Customized setuptools install command - prints a friendly greeting."""
+    user_options = install.user_options + [
+        ('jupyter-notebook-path=', None, 'Path to jupyter notebook')
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.jupyter_notebook_path = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        
     def run(self):
-        print("Hello, developer, how are you? :)")
-        print(self)
-        print(self.prefix)
+        folderPath = 'src/geppettoJupyter/geppetto/src/main/webapp/extensions/geppetto-neuron'
+        componentsInitializationTemplate = 'ComponentsInitialization_template.js'
+        componentsInitialization = 'ComponentsInitialization.js'
+        with open(os.path.join(folderPath, componentsInitialization),'w+') as file:
+            with open(os.path.join(folderPath, componentsInitializationTemplate), 'r') as templateFile:
+                data=templateFile.read().replace('var pythonNotebookPath = "http://localhost:8888/notebooks/Untitled.ipynb?kernel_name=python3";', 'var pythonNotebookPath = ' + self.jupyter_notebook_path + ';')
+                print(data)
+                file.write(data)
+        file.close()
+        templateFile.close()
+        
         install.run(self)
+        #self.do_egg_install()
 
 #raise AttributeError(data_files) For debug purposes
 
@@ -46,6 +67,3 @@ setuptools.setup(
     data_files=data_files,
     include_package_data=True,
 )
-
-
-#--install-option="--prefix='/usr/local'"
