11df3d4322a15d01ca6b9783230e96ec66f72dbb
Adrian Quintana <adrianquintana@gmail.com>
2016-10-02 01:09:03 +0100

    add method to install and enable module and to enable server extension, remove unneeded method to copy geppetto to nbextension and clean setup dict

--- setup.py
+++ setup.py
@@ -4,24 +4,43 @@ import fnmatch
 import os
 from setuptools.command.install import install
 import fileinput
+from notebook.nbextensions import install_nbextension_python, enable_nbextension_python
+from notebook.serverextensions import toggle_serverextension_python
 
 #This block copies resources to the server (/usr/local/share/jupyter/nbextensions/)
 data_files = []
-for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/css/'):
-    for filename in fnmatch.filter(filenames, '*'):
-        data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
-for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/less/'):
-    for filename in fnmatch.filter(filenames, '*'):
-        data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
-for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/images/'):
-    for filename in fnmatch.filter(filenames, '*'):
-        data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
-for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/js/'):
-    for filename in fnmatch.filter(filenames, '*'):
-        data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
+# for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/css/'):
+#     for filename in fnmatch.filter(filenames, '*'):
+#         data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
+# for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/less/'):
+#     for filename in fnmatch.filter(filenames, '*'):
+#         data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
+# for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/images/'):
+#     for filename in fnmatch.filter(filenames, '*'):
+#         data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
+# for root, dirnames, filenames in os.walk('src/geppettoJupyter/geppetto/src/main/webapp/js/'):
+#     for filename in fnmatch.filter(filenames, '*'):
+#         data_files.append(('share/jupyter/nbextensions' + root[3:].replace("src/main/webapp/", ""), [os.path.join(root, filename)]))
 
 data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.js')))
 data_files.append(('share/jupyter/nbextensions/geppettoJupyter', glob('src/geppettoJupyter/*.css')))
+
+# Installs the nbextension
+def run_nbextension_install(develop):
+    # Command: sudo jupyter nbextension install --py geppettoJupyter
+    print("Installing geppettoJupyter extension ...")
+    install_nbextension_python('geppettoJupyter', symlink=develop)
+
+    # Command: sudo jupyter nbextension enable --py geppettoJupyter
+    print("Enabling geppettoJupyter extensions ...")
+    isEnabled = enable_nbextension_python('geppettoJupyter')
+    if not isEnabled:
+        raise Exception('Problem enabling geppettoJupyter extension')
+    
+    # Command: jupyter serverextension enable --py geppettoJupyter
+    print("Enabling server extensions ...")
+    toggle_serverextension_python('geppettoJupyter', enabled=True)
+
 
 class CustomInstallCommand(install):
     user_options = install.user_options + [
@@ -36,9 +55,6 @@ class CustomInstallCommand(install):
         install.finalize_options(self)
         
     def run(self):
-        print("kaky")
-        print(self.jupyter_notebook_path)
-        
         if self.jupyter_notebook_path == None:
             self.jupyter_notebook_path = "'http://localhost:8888/notebooks/Untitled.ipynb?kernel_name=python3'";
         
@@ -51,7 +67,9 @@ class CustomInstallCommand(install):
                 print(line)    
         
         install.run(self)
-        #self.do_egg_install()
+        
+        print("Installing nbextension ...")
+        run_nbextension_install(False)
 
 #raise AttributeError(data_files) For debug purposes
 
@@ -60,13 +78,18 @@ setuptools.setup(
     cmdclass={
         'install': CustomInstallCommand,
     },
-    version='0.1.0',
-    url="http://geppetto.org",
-    author="Adrian Quintana",
-    description="Geppetto",
+    version="0.0.1",
+    url="https://github.com/openworm/org.geppetto.frontend.jupyter",
+    author="The Geppetto Development Team",
+    description="Geppetto extension for Jupyter notebook",
+    License= "MIT",
     long_description=open('README.md').read(),
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     data_files=data_files,
     include_package_data=True,
+    install_requires = [
+        'ipywidgets>=5.1.5',
+        'jupyter>=4.1.0'
+    ],
 )
