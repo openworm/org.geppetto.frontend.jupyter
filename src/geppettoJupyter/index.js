0763778ccb4311f94f9a7b70d550ef2a694917ee
Adrian Quintana <adrianquintana@gmail.com>
2016-11-09 13:17:37 +0000

    cleaning up

--- src/geppettoJupyter/index.js
+++ src/geppettoJupyter/index.js
@@ -1,11 +1,6 @@
-define(['base/js/namespace', './GeppettoWidgets', 'base/js/events', 'nbextensions/jupyter-js-widgets/extension'], function (Jupyter, GeppettoWidgets, events, JupyterWidgets) {
+define(['base/js/namespace', './GeppettoWidgets', 'base/js/events'], function (Jupyter, GeppettoWidgets, events) {
 
-	//'base/js/events',
-	
-	function load_ipython_extension(){
-		document.addEventListener("DOMContentLoaded", function(event) {
-			console.log('kake')
-		})
+	function load_ipython_extension() {
 		// Load css first
 		var $stylesheet = $('<link/>')
 			.attr({
@@ -16,9 +11,10 @@ define(['base/js/namespace', './GeppettoWidgets', 'base/js/events', 'nbextension
 			})
 			.appendTo('head');
 
-
+		// Hide the header
 		$('#header').hide();
 
+		// Add shortcut to hide/show header
 		IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-`', function (event) {
 			if (IPython.notebook.mode == 'command') {
 				$('#header').toggle();
@@ -27,15 +23,19 @@ define(['base/js/namespace', './GeppettoWidgets', 'base/js/events', 'nbextension
 			return true;
 		});
 
-		setTimeout(function(){if (!Jupyter.notebook) {
-	      events.on('notebook_loaded.Notebook', function(){Jupyter.notebook.get_cells()[0].execute();});
-	    } else {
-	    	Jupyter.notebook.get_cells()[0].execute();
-	    }}, 1000);
+		// Execute first cell.
+		// Due to a bug on jupyter, we have to wait for a second so that everything is loaded properly
+		setTimeout(function () {
+			if (!Jupyter.notebook) {
+				events.on('notebook_loaded.Notebook', function () { Jupyter.notebook.get_cells()[0].execute(); });
+			} else {
+				Jupyter.notebook.get_cells()[0].execute();
+			}
+		}, 1000);
 	}
-	
+
 	// Export the required load_ipython_extention
 	return {
-	    load_ipython_extension: load_ipython_extension
+		load_ipython_extension: load_ipython_extension
 	};
 });
