9705ec538964e419db928a751694a3124a8f5f71
Adrian Quintana <adrianquintana@gmail.com>
2016-11-15 23:08:56 +0000

    fix error about missing sync feature

--- src/geppettoJupyter/index.js
+++ src/geppettoJupyter/index.js
@@ -1,6 +1,6 @@
 define(['base/js/namespace', './GeppettoWidgets', 'base/js/events'], function (Jupyter, GeppettoWidgets, events) {
 
-	function customise() {
+	function load_extension() {
 		// Load css first
 		var $stylesheet = $('<link/>')
 			.attr({
@@ -23,55 +23,20 @@ define(['base/js/namespace', './GeppettoWidgets', 'base/js/events'], function (J
 			return true;
 		});
 
+		// Restart kernel to delete any previous variable
+		//IPython.notebook.kernel.restart(function(){
+			// Once restarted, execute all the cells
+		//	IPython.notebook.execute_all_cells();
+		//});
 
-
-
-			// Restart kernel to delete any previous variable
-			// console.log("GPT: Restart the server");
-			IPython.notebook.kernel.restart(function(){
-				console.log("GPT: Kernel is ready");
-				IPython.notebook.execute_all_cells();
-			});
-
-			// // Execute all cells
-			// $(IPython.events).on('kernel_ready.Kernel', function(){
-			// 	console.log("GPT: Kernel is ready");
-			// 	IPython.notebook.execute_all_cells();
-			// });
-
-
-
-
-
-
-
-			// console.log("GPT: Restart the server");
-			// IPython.notebook.restart_kernel().then(function() {
-			// 	console.log("GPT: Kernel is ready. Executing all the cells");
-            // 	IPython.notebook.execute_all_cells();
-            //      });
- 
-
-		
-		
-
-		// Execute first cell.
-		// Due to a bug on jupyter, we have to wait for a second so that everything is loaded properly
-		// setTimeout(function () {
-		// 	if (!Jupyter.notebook) {
-		// 		events.on('notebook_loaded.Notebook', function () { Jupyter.notebook.get_cells()[0].execute(); });
-		// 	} else {
-		// 		Jupyter.notebook.get_cells()[0].execute();
-		// 	}
-		// }, 1000);
+		IPython.notebook.restart_run_all({confirm: false})
 	}
 
 	 var load_ipython_extension = function () {
-        
         if (IPython.notebook) {
-            customise();
+            load_extension();
         }
-        $([IPython.events]).on("notebook_loaded.Notebook", customise);
+        $([IPython.events]).on("notebook_loaded.Notebook", load_extension);
     };
 
 	// Export the required load_ipython_extention
