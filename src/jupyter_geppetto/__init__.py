4e2abda4ca7eb68d7ad868913d2fc9ec77196dbf
Adrian Quintana <adrianquintana@gmail.com>
2017-04-03 20:02:31 +0100

    create initial ipynb on init

--- src/jupyter_geppetto/__init__.py
+++ src/jupyter_geppetto/__init__.py
@@ -4,6 +4,14 @@ from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 import tornado.websocket
 import tornado.web
+import json
+import requests
+import nbformat as nbf
+import codecs
+import os.path
+
+from nbformat.v4.nbbase import new_notebook
+
 
 def _jupyter_server_extension_paths():
     return [{
@@ -11,6 +19,8 @@ def _jupyter_server_extension_paths():
     }]
 
 # Jupyter Extension points
+
+
 def _jupyter_nbextension_paths():
     return [dict(
         section="notebook",
@@ -21,30 +31,49 @@ def _jupyter_nbextension_paths():
         # _also_ in the `nbextension/` namespace
         require="jupyter_geppetto/index")]
 
+
 class GeppettoHandler(IPythonHandler):
+
     def get(self):
-        template = os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/templates/dist/geppetto.vm')
+        # Create initial ipynb if it doesn't exist
+        if not os.path.isfile('neuron-ui-demo.ipynb'):
+            nb0 = new_notebook(cells=[],
+                               metadata={'language': 'python',})
+            f = codecs.open('neuron-ui-demo.ipynb', encoding='utf-8', mode='w')
+            nbf.write(nb0, f, 4)
+            f.close()
+
+        template = os.path.join(os.path.dirname(
+            __file__), 'geppetto/src/main/webapp/templates/dist/geppetto.vm')
         self.write(open(template).read())
 
+
 class GeppettoProjectsHandler(IPythonHandler):
+
     def get(self):
         self.write({})
-        
+
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+
     def open(self):
         # 1 -> Send the connection
-        self.write_message({"type":"client_id", "data":"{\"clientID\":\"Connection1\"}"})
+        self.write_message(
+            {"type": "client_id", "data": "{\"clientID\":\"Connection1\"}"})
         # 2 -> Check user privileges
-        self.write_message({"type":"user_privileges", "data":"{\"user_privileges\": \"{\\\"userName\\\":\\\"Python User\\\",\\\"loggedIn\\\":true,\\\"hasPersistence\\\":false,\\\"privileges\\\":[\\\"READ_PROJECT\\\",\\\"DOWNLOAD\\\",\\\"DROPBOX_INTEGRATION\\\", \\\"RUN_EXPERIMENT\\\", \\\"WRITE_PROJECT\\\"]}\"}"})
+        self.write_message(
+            {"type": "user_privileges", "data": "{\"user_privileges\": \"{\\\"userName\\\":\\\"Python User\\\",\\\"loggedIn\\\":true,\\\"hasPersistence\\\":false,\\\"privileges\\\":[\\\"READ_PROJECT\\\",\\\"DOWNLOAD\\\",\\\"DROPBOX_INTEGRATION\\\", \\\"RUN_EXPERIMENT\\\", \\\"WRITE_PROJECT\\\"]}\"}"})
 
     def on_message(self, message):
         jsonMessage = json.loads(message)
         if (jsonMessage['type'] == 'geppetto_version'):
-            #Where do we get the geppetto version from?
-            self.write_message({"requestID":jsonMessage['requestID'],"type":"geppetto_version","data":"{\"geppetto_version\":\"0.3.3\"}"});
+            # Where do we get the geppetto version from?
+            self.write_message({"requestID": jsonMessage[
+                               'requestID'], "type": "geppetto_version", "data": "{\"geppetto_version\":\"0.3.3\"}"})
 
     def on_close(self):
         pass
+
 
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("Geppetto Jupyter extension is running!")
@@ -54,10 +83,14 @@ def load_jupyter_server_extension(nbapp):
     route_pattern = url_path_join(web_app.settings['base_url'], '/geppetto')
     web_app.add_handlers(host_pattern, [(route_pattern, GeppettoHandler)])
 
-    route_pattern_geppetto_projects = url_path_join(web_app.settings['base_url'], '/geppettoprojects')
-    web_app.add_handlers(host_pattern, [(route_pattern_geppetto_projects, GeppettoProjectsHandler)])
+    route_pattern_geppetto_projects = url_path_join(
+        web_app.settings['base_url'], '/geppettoprojects')
+    web_app.add_handlers(
+        host_pattern, [(route_pattern_geppetto_projects, GeppettoProjectsHandler)])
 
-    websocket_pattern = url_path_join(web_app.settings['base_url'], '/org.geppetto.frontend/GeppettoServlet')
+    websocket_pattern = url_path_join(
+        web_app.settings['base_url'], '/org.geppetto.frontend/GeppettoServlet')
     web_app.add_handlers(host_pattern, [(websocket_pattern, WebSocketHandler)])
 
-    web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
+    web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {
+                         'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
