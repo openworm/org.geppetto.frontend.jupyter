cc4b9cfcc28e6baf6b0c37b099de965b03169d88
Adrian Quintana <adrianquintana@gmail.com>
2018-10-07 19:54:18 +0100

    make jupyter_geppetto generic

--- src/jupyter_geppetto/__init__.py
+++ src/jupyter_geppetto/__init__.py
@@ -6,17 +6,16 @@ from notebook.base.handlers import IPythonHandler
 import tornado.websocket
 import tornado.web
 import nbformat as nbf
-
+import logging
 from nbformat.v4.nbbase import new_notebook
+from tornado.routing import PathMatches
+import pkg_resources
 
 
 def _jupyter_server_extension_paths():
     return [{
         "module": "jupyter_geppetto"
     }]
-
-# Jupyter Extension points
-
 
 def _jupyter_nbextension_paths():
     return [dict(
@@ -32,16 +31,26 @@ def _jupyter_nbextension_paths():
 class GeppettoHandler(IPythonHandler):
 
     def get(self):
-        # Create initial ipynb if it doesn't exist
-        if not os.path.isfile('notebook.ipynb'):
-            nb0 = new_notebook(cells=[],
-                               metadata={'language': 'python',})
-            f = codecs.open('notebook.ipynb', encoding='utf-8', mode='w')
-            nbf.write(nb0, f, 4)
-            f.close()
+        # id=self.get_argument("id", None)
+        id = "jupyter_geppetto"
+        if id:
+            # Create initial ipynb if it doesn't exist
+            if not os.path.isfile('notebook.ipynb'):
+                nb0 = new_notebook(cells=[],
+                                metadata={'language': 'python',})
+                f = codecs.open('notebook.ipynb', encoding='utf-8', mode='w')
+                nbf.write(nb0, f, 4)
+                f.close()
 
-        template = os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/build/geppetto.vm')
-        self.write(open(template).read())
+            
+            path = 'geppetto/src/main/webapp/build/geppetto.vm'  # always use slash
+            # template = pkg_resources.resource_filename('jupyter_geppetto', path)
+            template = pkg_resources.resource_filename(id, path)
+            # template = os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/build/geppetto.vm')
+            self.write(open(template).read())
+        else:
+            self.log.warning('Package to load missing in the url')
+            self.write('Package to load missing in the url')
 
 
 class GeppettoProjectsHandler(IPythonHandler):
@@ -72,23 +81,44 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 
 def load_jupyter_server_extension(nbapp):
-    nbapp.log.info("Geppetto Jupyter extension is running!")
+    try:
+        
+        nbapp.log.info("Geppetto Jupyter extension is running!")
 
-    web_app = nbapp.web_app
-    host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/geppetto')
-    web_app.add_handlers(host_pattern, [(route_pattern, GeppettoHandler)])
+        path = 'geppetto/src/main/webapp/'  # always use slash
+        template2 = pkg_resources.resource_filename('jupyter_geppetto', path)
 
-    route_pattern_geppetto_projects = url_path_join(
-        web_app.settings['base_url'], '/geppettoprojects')
-    web_app.add_handlers(
-        host_pattern, [(route_pattern_geppetto_projects, GeppettoProjectsHandler)])
+        web_app = nbapp.web_app
+        host_pattern = '.*$'
+        route_pattern = url_path_join(web_app.settings['base_url'], '/geppetto')
+        web_app.add_handlers(host_pattern, [(route_pattern, GeppettoHandler)])
 
-    websocket_pattern = url_path_join(
-        web_app.settings['base_url'], '/org.geppetto.frontend/GeppettoServlet')
-    web_app.add_handlers(host_pattern, [(websocket_pattern, WebSocketHandler)])
+        route_pattern_geppetto_projects = url_path_join(
+            web_app.settings['base_url'], '/geppettoprojects')
+        web_app.add_handlers(
+            host_pattern, [(route_pattern_geppetto_projects, GeppettoProjectsHandler)])
 
-    web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {
-                         'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
-    
-    web_app.add_handlers(host_pattern, [(r"/org.geppetto.frontend/geppetto/(.*)", tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
+        websocket_pattern = url_path_join(
+            web_app.settings['base_url'], '/org.geppetto.frontend/GeppettoServlet')
+        web_app.add_handlers(host_pattern, [(websocket_pattern, WebSocketHandler)])
+
+        nbapp.log.info(os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/'))
+        nbapp.log.info(type(os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')))
+        nbapp.log.info(template2)
+        nbapp.log.info(type(template2))
+
+        # web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {
+        #                     'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
+        web_app.add_handlers(host_pattern, [(r"/geppetto/(.*)", tornado.web.StaticFileHandler, {
+                             'path': template2})])
+        
+
+
+        nbapp.log.info(os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/'))
+        nbapp.log.info(template2)
+        # web_app.add_handlers(host_pattern, [(r"/org.geppetto.frontend/geppetto/(.*)", tornado.web.StaticFileHandler, {
+        #     'path': os.path.join(os.path.dirname(__file__), 'geppetto/src/main/webapp/')})])
+        web_app.add_handlers(host_pattern, [(r"/org.geppetto.frontend/geppetto/(.*)", tornado.web.StaticFileHandler, {
+            'path': template2})])
+    except:
+        nbapp.log.info('error')
