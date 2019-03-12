526d38956da87a5483a58f4c1e3e23c4a7d4c85b
Filippo Ledda <filippo@metacell.us>
2019-03-12 15:49:31 +0100

    Fixed error when enabling outside root folder

--- jupyter_geppetto/service.py
+++ jupyter_geppetto/service.py
@@ -1,16 +1,20 @@
 from jupyter_geppetto import settings
 from notebook.utils import url_path_join
 import logging
-
+import os
 class PathService:
-    webapp_directory = ''
+    webapp_directory = './webapp/'
 
     @classmethod
     def get_webapp_directory(cls):
-        if not cls.webapp_directory:
-            import glob, os
-            cls.webapp_directory = os.path.dirname(glob.glob('*/' + settings.geppetto_webapp_file)[0])
-            logging.info('Webapp directory discovered: {}'.format(cls.webapp_directory))
+        if not os.path.exists(cls.webapp_directory):
+            import glob
+            discovered_paths = glob.glob('*/' + settings.geppetto_webapp_file)
+            if discovered_paths:
+                cls.webapp_directory = os.path.dirname([0])
+                logging.info('Webapp directory discovered: {}'.format(cls.webapp_directory))
+            else:
+                logging.error('Cannot determine webapp directory. PathService won\'t work')
         return cls.webapp_directory
 
     @classmethod
