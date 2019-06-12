4d3b963e9f25666063015ff2bdc183f3286a0d75
Filippo Ledda <filippo@metacell.us>
2019-06-12 17:26:42 +0200

    Removed Geppetto workflow logic calls. Removed also kernel restart.

--- js/src/index.js
+++ js/src/index.js
@@ -1,51 +1,93 @@
-import './jupyter_geppetto';
-import './geppettoJupyter.less';
+var jupyter_widgets = require('@jupyter-widgets/base');
+var GEPPETTO = window.parent.GEPPETTO;
+import _ from 'underscore';
 
-function load_extension() {
+define('jupyter_geppetto', function () {
 
-	// Hide the header
-	$('#header').hide();
+	var ComponentSync = jupyter_widgets.WidgetModel.extend({
+		defaults: _.extend({}, jupyter_widgets.WidgetModel.prototype.defaults, {
+			_model_name: 'ComponentSync',
+			_model_module: "jupyter_geppetto",
+			_model_module_version : '~1.0.0',
+			value: undefined,
+			parent: null,
+			componentType: undefined
+		}),
 
-	// Add shortcut to hide/show header
-	IPython.keyboard_manager.command_shortcuts.add_shortcut('ctrl-`', function (event) {
-		if (IPython.notebook.mode == 'command') {
-			$('#header').toggle();
-			return false;
+		initialize: function (options) {
+			ComponentSync.__super__.initialize.apply(this, arguments);
+
+			this.on("msg:custom", this.handle_custom_messages, this);
+			this.on("change:value", this.handle_value_change, this);
+
+		},
+
+		syncValueWithPython: function (value) {
+			var jsonValue = JSON.stringify(value);
+			this.set('value', jsonValue);
+			this.save_changes();
+			this.send({ event: 'sync_value', value: jsonValue });
+		},
+
+		getParameters: function (parameters) {
+			parameters['id'] = this.get('widget_id');
+			parameters['name'] = this.get('widget_name');
+			parameters['value'] = this.get('value');
+			parameters['syncValueWithPython'] = this.syncValueWithPython;
+			parameters['isStateless'] = true;
+			parameters['read_only'] = false;
+			return parameters;
+		},
+
+		createComponent: function (componentItem, parameters, container) {
+
+			var component = GEPPETTO.ComponentFactory._addComponent(componentItem, this.componentType, this.getParameters(parameters),
+				container, undefined, (this.get("embedded") == false));
+			this.component = component;
+
+			return component;
+		},
+
+		handle_value_change: function (model, jsonValue, options) {
+			var value = "";
+			if (jsonValue != undefined && jsonValue != "") {
+				value = JSON.parse(jsonValue);
+			}
+			if (model.get('parent') != null) {
+				model.get('parent').forceRender();
+			}
+			else {
+				if ((this.component != undefined) && (this.component._isMounted == true)) {
+					if (this.component.state.value !== value || this.component.state.searchText !== value) {
+						this.component.setState({ value: value, searchText: value, checked: (value || value == "True") });
+					}
+				}
+			}
+		},
+
+		handle_custom_messages: function (msg) {
+			if (msg.type === 'connect') {
+				var component = GEPPETTO.ComponentFactory.getComponentById(this.get("componentType"), this.id);
+				//this could be undefined if we are in the middle of a rename
+				if (component != undefined) {
+					component.setSyncValueWithPythonHandler(this.syncValueWithPython.bind(this));
+					this.component = component;
+				}
+
+			}
+			else if (msg.type === 'disconnect') {
+				this.off("msg:custom", this.handle_custom_messages, this);
+				this.off("change:value", this.handle_value_change, this);
+				//this could be undefined if we are in the middle of a rename
+				if (this.component != undefined) {
+					this.component.setSyncValueWithPythonHandler(null);
+					this.component = null;
+				}
+			}
 		}
-		return true;
 	});
 
-	// Make Jupyter (aka IPython) available to Geppetto
-	window.parent.IPython = IPython;
-
-	// If a Geppetto extension is defining a custom behavior to load the kernel we call it
-	IPython.notebook.restart_kernel({ confirm: false }).then(function () {
-		
-		//Import the GUI sync to use the Python Controlled Capabilities, logging, etc
-		IPython.notebook.kernel.execute('from jupyter_geppetto import jupyter_geppetto, utils');
-
-		// Load the project & activate the experiment
-		var project = { id: 1, name: 'Project', experiments: [{ "id": 1, "name": 'Experiment', "status": 'DESIGN' }] }
-		window.parent.GEPPETTO.Manager.loadProject(project, false);
-		window.parent.GEPPETTO.Manager.loadExperiment(1, [], []);
-
-		// Trigger event for the extension (ComponentInitialization) to run custom code
-		window.parent.GEPPETTO.trigger('jupyter_geppetto_extension_ready')
-	});
-}
-
-var load_ipython_extension = function () {
-	if (IPython.notebook !== undefined && IPython.notebook._fully_loaded) {
-		console.log("Kernel ready")
-		load_extension();
-	} else {
-		console.log("Waiting for kernel to be ready")
-		IPython.notebook.events.on("notebook_loaded.Notebook", function () {
-			load_extension();
-		})
-	}
-};
-
-export {
-	load_ipython_extension
-};
+	return {
+		ComponentSync: ComponentSync
+	};
+});
