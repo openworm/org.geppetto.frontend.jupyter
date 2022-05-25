0a8c89cadbb1184eb9059d6a7347daee6b0f75cb
Filippo Ledda <46561561+filippomc@users.noreply.github.com>
2022-05-25 11:53:55 +0200

    Bump up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ data_files.append(('share/jupyter/nbextensions/jupyter_geppetto', glob('jupyter_
 
 setuptools.setup(
     name="jupyter_geppetto",
-    version="1.1.1",
+    version="1.1.2",
     url="https://github.com/openworm/org.geppetto.frontend.jupyter",
     author="The Geppetto Development Team",
     author_email="info@geppetto.org",
